92fdcbde5124ed6a2b94aee0db3cfb48b0f0fbc8
wassim <wassimglouia@hotmail.com>
2022-05-27 14:47:43 +0200

    influencer project

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Col from 'react-bootstrap/esm/Col';
 import Container from 'react-bootstrap/esm/Container';
 import Row from 'react-bootstrap/esm/Row';
 import { Link } from 'react-router-dom';
 import './home.css';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 const Home = () => {
+  useEffect(()=>{
+    Aos.init({duration: 2000});
+      },[]  );
   return (
     <div>
     <Container>
@@ -44,25 +49,30 @@ const Home = () => {
             <div className='containe'>
             <div className='row'>
             <ul className='sec'>
-            <li style={{padding: 15}}>
-            <img src="https://www.influence4you.com/images/v10/starbuck-logo.svg" alt="Starbucks" width="53" height="88"/>
+            <li  style={{padding: 15}}>
+            <img data-aos="fade-right" src="https://www.influence4you.com/images/v10/starbuck-logo.svg" alt="Starbucks" width="53" height="88"/>
             </li>
-            
             <li style={{padding: 15}}>
-            <img src="https://www.influence4you.com/images/v10/dior-logo.svg" alt="Dior" width="89" height="88"/>
+            <img data-aos="zoom-in" src="https://influencity.com/static/images/aboutUs/brands/Sansung-logo.jpg" alt="Dior" width="89" height="88"/>
+            </li> 
+            <li style={{padding: 15}}>
+            <img data-aos="zoom-in" src="https://www.influence4you.com/images/v10/dior-logo.svg" alt="Dior" width="89" height="88"/>
             </li>            
             <li style={{padding: 15}}>
-            <img src="https://www.influence4you.com/images/v10/eBay-logo.svg" alt="eBay" width="82" height="88"/>            </li>
+            <img data-aos="zoom-in" src="https://influencity.com/static/images/aboutUs/brands/Upfield-logo.jpg" alt="eBay" width="82" height="88"/>            </li>
             <li style={{padding: 15}}>
-            <img src="https://www.influence4you.com/images/v10/Lacoste-logo.svg" alt="Lacoste" width="109" height="88"/>            </li>
+            <img data-aos="zoom-in" src="https://www.influence4you.com/images/v10/Lacoste-logo.svg" alt="Lacoste" width="109" height="88"/>            </li>
             <li style={{padding: 15}}>
-            <img src="https://www.influence4you.com/images/v10/prime-video-icon.svg" alt="Amazon Prime Video" width="148" height="88"/>            </li>
+            <img data-aos="zoom-in" src="https://influencity.com/static/images/aboutUs/brands/Aliexpress-logo.jpg" alt="Dior" width="89" height="88"/>
+            </li> 
             <li style={{padding: 15}}>
-            <img src="https://www.influence4you.com/images/v10/lego-logo.svg" alt="Lego" width="70" height="88"/>            </li>
+            <img data-aos="zoom-in" src="https://www.influence4you.com/images/v10/prime-video-icon.svg" alt="Amazon Prime Video" width="148" height="88"/>            </li>
             <li style={{padding: 15}}>
-            <img src="https://www.influence4you.com/images/v10/tinder-logo.svg" alt="Tinder" width="49" height="88"/>            </li>
+            <img data-aos="zoom-in" src="https://influencity.com/static/images/aboutUs/brands/Reprise-logo.jpg" alt="Lego" width="70" height="88"/>            </li>
             <li style={{padding: 15}}>
-            <img width="70" height="70" src="https://findly.co/wp-content/uploads/2021/02/Sodebo-300x121.png" data-src="https://findly.co/wp-content/uploads/2021/02/Sodebo-300x121.png" class="attachment-medium size-medium lazyloaded" alt="Sodebo client Influentia"/>            </li>
+            <img data-aos="zoom-in" src="https://www.influence4you.com/images/v10/tinder-logo.svg" alt="Tinder" width="49" height="88"/>            </li>
+            <li style={{padding: 15}}>
+            <img data-aos="fade-left" width="70" height="70" src="https://influencity.com/static/images/aboutUs/brands/Octagon-logo.jpg" data-src="https://findly.co/wp-content/uploads/2021/02/Sodebo-300x121.png" class="attachment-medium size-medium lazyloaded" alt="Sodebo client Influentia"/>            </li>
 
 </ul>
             </div>
@@ -75,41 +85,42 @@ const Home = () => {
         <Row className='comm'>
        
         <Col>
-  <div> <p>Nous avons décidé de prendre la plateforme Findly pour optimiser nos campagnes. Le module de Recherche est vraiment un gain de temps incroyable et très pratique. </p>
+  <div className='cant'> <p>Nous avons décidé de prendre la plateforme Findly pour optimiser nos campagnes. Le module de Recherche est vraiment un gain de temps incroyable et très pratique. </p>
+ <div className='cama'>
   <div className='nom'>
-  <img className='avat' src="https://findly.co/wp-content/uploads/2022/04/thara-150x150.webp" data-src="https://findly.co/wp-content/uploads/2022/04/thara-150x150.webp" class="lae-image full ls-is-cached lazyloaded" alt=""/>
-  
-   
+  <img  src="https://findly.co/wp-content/uploads/2022/04/thara-150x150.webp" />
+  </div>
      <div className='name'>
-     Thara Tremblay-Nantel <br/>
-    <p1>Fondatrice</p1>
+     Thara Tremblay-Nantel  <br/>
+    <p className='parae'>Fondatrice</p>
 
+     </div>     
      </div>
-     
-     
-   </div>
    </div>
    </Col>
+
    <Col>
-  <div> <p>
-  La plateforme Influencer est vite devenu un indispensable pour nos équipes ! Et un grand bravo aux chefs de projets qui restent disponibles pour la moindre question !
-          </p>
+  <div className='cant'> <p> La plateforme Influencer est vite devenu un indispensable pour nos équipes ! Et un grand bravo aux chefs de projets qui restent disponibles pour la moindre question ! </p>
+  <div className='cama'>
           <div className='nom'>
-          <img className='avat' src="https://findly.co/wp-content/uploads/2022/04/remy-150x150.webp" data-src="https://findly.co/wp-content/uploads/2022/04/remy-150x150.webp" class="lae-image full lazyloaded" alt="" data-srcset="https://findly.co/wp-content/uploads/2022/04/remy-150x150.webp 150w, https://findly.co/wp-content/uploads/2022/04/remy.webp 200w" data-sizes="(max-width: 150px) 100vw, 150px" sizes="(max-width: 150px) 100vw, 150px" srcset="https://findly.co/wp-content/uploads/2022/04/remy-150x150.webp 150w, https://findly.co/wp-content/uploads/2022/04/remy.webp 200w"/>
+          <img  src="https://findly.co/wp-content/uploads/2022/04/remy-150x150.webp" />
+          </div>
           <div className='name'>
           Remy Siriex - Agence Anouk  <br/>
-    <p1>Community Manager</p1>
+    <p className='parae'>Community Manager</p>
 </div>
      </div>
   </div>
   </Col>
   <Col>
-  <div> <p> Un superbe outil d’analyse de campagnes de marketing d’influence. Les équipes sont disponibles et réactives ! Trouvez facilement les influenceurs pertinents</p>   
+  <div className='cant'> <p> Un superbe outil d’analyse de campagnes de marketing d’influence. Les équipes sont disponibles et réactives ! Trouvez facilement les influenceurs pertinents</p>   
+  <div className='cama'>
   <div className='nom'>
-  <img className='avat' src="https://findly.co/wp-content/uploads/2022/04/kim-150x150.webp" data-src="https://findly.co/wp-content/uploads/2022/04/kim-150x150.webp" class="lae-image full lazyloaded" alt="kim" data-srcset="https://findly.co/wp-content/uploads/2022/04/kim-150x150.webp 150w, https://findly.co/wp-content/uploads/2022/04/kim.webp 200w" data-sizes="(max-width: 150px) 100vw, 150px" sizes="(max-width: 150px) 100vw, 150px" srcset="https://findly.co/wp-content/uploads/2022/04/kim-150x150.webp 150w, https://findly.co/wp-content/uploads/2022/04/kim.webp 200w"></img>
+  <img  src="https://findly.co/wp-content/uploads/2022/04/kim-150x150.webp" ></img>
+  </div>
   <div className='name'>
     Kim Hua - Wellcom​ <br/>
-    <p1>Consultante Influence Marketing</p1>
+    <p className='parae'>Consultante  Marketing</p>
 </div>
      </div>
     </div>
